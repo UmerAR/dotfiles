0d0b132e6a033d4240d28520eb99ec8b1807eacc
UmerAR <huda.umer@gmail.com>
2026-04-14 19:21:31 +0400

    auto update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -913,6 +913,11 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  {
+    'm4xshen/autoclose.nvim',
+    opts = {},
+  },
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
